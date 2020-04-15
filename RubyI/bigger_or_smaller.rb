0d69895645fcc0f566f00f7ee47f837801018ce0
Ayrton Aoki <ayrtonaoki@Ayrtons-MacBook-Air.local>
2020-04-15 01:57:16 -0300

    Update bigger or smaller game

--- RubyI/bigger_or_smaller.rb
+++ RubyI/bigger_or_smaller.rb
@@ -1,27 +1,45 @@
-puts "Welcome to Bigger or Smaller - The Game"
-puts "\n"
-puts "What's your name?"
-name = gets
+def welcome
+  puts "Welcome to Bigger or Smaller - The Game"
+  puts "\n"
+  puts "What's your name?"
+  name = gets
+  puts "Let's start the game, #{name}"
+end
 
-puts "Let's start the game, " + name
+def choose_number(attempt, attempt_limit, numbers_tried)
+  puts "\n"
+  puts "Choose a number between 1 and 100 - Attempt " +
+    "#{attempt.to_s} of  #{attempt_limit.to_s}"
+  puts "Attemps so far: #{numbers_tried.to_s}"
+  shot = gets
+  shot.to_i
+end
+
+def check_shot(shot, secret_mumber)
+  puts "\n"
+  if shot == secret_mumber
+    puts "Hit :)"
+    return true
+  end
+
+  puts "Miss :("
+  if secret_mumber > shot
+    puts "The secret number is bigger!"
+  else
+    puts "The secret number is smaller!"
+  end
+  false
+end
+
+welcome
 
 secret_mumber = 22
 attempt_limit = 5
+numbers_tried = []
 
 for attempt in 1..attempt_limit do
-  puts "\n"
-  puts "Choose a number between 1 and 100 - Attempt " + attempt.to_s + ":"
-  shot = gets
-  puts "\n"
-  if shot.to_i == secret_mumber
-    puts "Hit :)"
-    break
-  else
-    puts "Miss :("
-    if secret_mumber > shot.to_i
-      puts "The secret number is bigger!"
-    else
-      puts "The secret number is smaller!"
-    end
-  end
+  shot = choose_number attempt, attempt_limit, numbers_tried
+  numbers_tried << shot
+
+  break if check_shot(shot, secret_mumber)
 end
