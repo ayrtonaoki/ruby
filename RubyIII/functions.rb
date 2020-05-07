ba3c3e5508bc7766edcfcdc5f1c743b3587ab1b1
Ayrton Aoki <ayrtonaoki@Ayrtons-MacBook-Air.local>
2020-05-07 00:35:18 -0300

    Create find_player_spot method

--- RubyIII/functions.rb
+++ RubyIII/functions.rb
@@ -2,3 +2,12 @@ def read_map(map_number)
   file = File.read "map#{map_number}.txt"
   file.split("\n")
 end
+
+def find_player_spot(map)
+  map.each_with_index do |map_row, row_index|
+    player_column = map_row.index "H"
+    if player_column
+      return row_index, player_column
+    end
+  end
+end
