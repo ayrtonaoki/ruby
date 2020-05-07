369557fc28e48c4cb17b45d430ade25203a1c02c
Ayrton Aoki <ayrtonaoki@Ayrtons-MacBook-Air.local>
2020-05-07 00:36:28 -0300

    Create player movement on map

--- RubyIII/main.rb
+++ RubyIII/main.rb
@@ -5,6 +5,25 @@ require_relative "functions"
 
 loop do
   system "clear"
-  puts read_map(choose_map)
+  map = read_map(choose_map)
+  puts map
+  while true
+    puts "\n"
+    player_movement = choose_player_movement
+    player_spot = find_player_spot(map)
+    map[player_spot[0]][player_spot[1]] = " "
+    case player_movement
+    when "W"
+      player_spot[0] -= 1
+    when "S"
+      player_spot[0] += 1
+    when "A"
+      player_spot[1] -= 1
+    when "D"
+      player_spot[1] += 1
+    end
+    map[player_spot[0]][player_spot[1]] = "H"
+    puts map
+  end
   break if !play_again
 end
