f1d52dd6d94b509a129eb498d2199d54c0223dc1
Ayrton Aoki <ayrtonaoki@Ayrtons-MacBook-Air.local>
2020-05-06 23:01:16 -0300

    Update file, refactory code

--- RubyIII/ui.rb
+++ RubyIII/ui.rb
@@ -30,17 +30,10 @@ end
 
 def choose_map
   puts "Choose your map:"
-  puts "(1)"
-  map1 = File.read "map1.txt"
-  puts map1.split("\n")
-  puts "\n"
-  puts "(2)"
-  map2 = File.read "map2.txt"
-  puts map2.split("\n")
-  puts "\n"
-  puts "(3)"
-  map3 = File.read "map3.txt"
-  puts map3.split("\n")
+  for i in 1..3 do
+    puts "\n(#{i})"
+    puts read_map(i)
+  end
   puts "\n"
   map = gets.strip
 end
