3394af964ae79224d56da20e4a7a811258f3b94f
Ayrton Aoki <ayrtonaoki@Ayrtons-MacBook-Air.local>
2020-05-16 04:45:51 -0300

    Update code, refacture

--- RubyIII/main.rb
+++ RubyIII/main.rb
@@ -6,19 +6,14 @@ require_relative "functions"
 loop do
   system "clear"
   map = read_map(choose_map)
-  puts map
   while true
+    puts map
     puts "\n"
-    player_movement = choose_player_movement
     player_position = find_player(map)
     new_player_position = set_player_position(player_movement, player_position)
-    if map[new_player_position[0]][new_player_position[1]] == "#"
-      puts map
-      next
-    end
+    next if !position_is_valid?(map, new_player_position)
     map[player_position[0]][player_position[1]] = " "
     map[new_player_position[0]][new_player_position[1]] = "H"
-    puts map
   end
   break if !play_again
 end
